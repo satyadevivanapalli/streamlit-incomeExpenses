b94827951636f2c0ed8f61b40199ef1befaefd4d
satyadevivanapalli <satyadevi.vanapalli@techvedika.com>
2024-03-21 11:21:16 +0530

    made changes in example and secrets

--- example.py
+++ example.py
@@ -52,7 +52,7 @@ import streamlit as st
 # except Exception as e:
 #       st.write(e)
 
-conn = st.connection('authentication1', type='sql')
+conn = st.connection('mysql', type='sql')
 st.write(conn,"+++++++++++++++++++++++=")
 df = conn.query('SELECT * from users;', ttl=0)
 # st.write(df,"ffffffffffffffffffffffffff")
